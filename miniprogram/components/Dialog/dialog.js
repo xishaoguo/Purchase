19a4b46f6de757dc76fc6d67d2ccbcc6c0c79993
songyakai <songyakai@sdiread.com>
2019-04-23 16:16:26 +0800

    列表实现

---
+++ miniprogram/components/Dialog/dialog.js
@@ -0,0 +1,71 @@
+// components/Dialog/dialog.js
+Component({
+  options: {
+    multipleSlots: true // 在组件定义时的选项中启用多slot支持
+  },
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    // 弹窗标题
+    title: {            // 属性名
+      type: String,     
+      value: '标题'     // 属性初始值（可选），如果未指定则会根据类型选择一个
+    },
+    // 弹窗内容
+    content: {
+      type: String,
+      value: '弹窗内容'
+    },
+    // 弹窗取消按钮文字
+    cancelText: {
+      type: String,
+      value: '返回'
+    },
+    // 弹窗确认按钮文字
+    confirmText: {
+      type: String,
+      value: '加入购物车'
+    } 
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    isShow: false,
+    brand: "品牌1",
+    specifications: "11",
+    explain: "这是个好东西"
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    //隐藏弹框
+    hideDialog() {
+      this.setData({
+        isShow: !this.data.isShow
+      })
+    },
+    //展示弹框
+    showDialog() {
+      this.setData({
+        isShow: !this.data.isShow
+      })
+    },
+    /*
+    * 内部私有方法建议以下划线开头
+    * triggerEvent 用于触发事件
+    */
+    _cancelEvent() {
+      //触发取消回调
+      this.triggerEvent("cancelEvent")
+    },
+    _confirmEvent() {
+      //触发成功回调
+      this.triggerEvent("confirmEvent");
+    }
+  }
+})
---
+++ miniprogram/components/Listitem/listitem.js
@@ -0,0 +1,25 @@
+// components/listitem/listitem.html.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    brand:"品牌1",
+    specifications:"11",
+    explain:"这是个好东西"
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
